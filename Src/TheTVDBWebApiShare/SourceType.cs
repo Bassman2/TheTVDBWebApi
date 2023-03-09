2b72cd3c490afdde26fd9fabce163115fd792e94
Bassman2 <ralf.beckers@hotmail.com>
2023-03-09 21:58:20 +0100

    Update

---
+++ Src/TheTVDBWebApiShare/SourceType.cs
@@ -0,0 +1,26 @@
+﻿namespace TheTVDBWebApiShare
+{
+    /// <summary>
+    /// Source type record.
+    /// </summary>
+    public class SourceType
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("postfix")]
+        public string Postfix { get; set; }
+
+        [JsonPropertyName("prefix")]
+        public string Prefix { get; set; }
+
+        [JsonPropertyName("slug")]
+        public string Slug { get; set; }
+
+        [JsonPropertyName("sort")]
+        public long Sort { get; set; }
+    }
+}
--- Src/TheTVDBWebApiShare/TVDBWeb.Source.cs
+++ Src/TheTVDBWebApiShare/TVDBWeb.Source.cs
@@ -2,5 +2,15 @@
 {
     public partial class TVDBWeb
     {
+        /// <summary>
+        /// Returns list of source type records.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>List of source type records</returns>
+        public async Task<List<SourceType>> GetSourceTypesAsync(CancellationToken cancellationToken = default)
+        {
+            Response<List<SourceType>> resp = await GetAsync<List<SourceType>>($"v4/series/statuses", cancellationToken);
+            return resp.Data;
+        }
     }
 }
