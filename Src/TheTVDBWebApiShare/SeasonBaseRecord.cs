3ab20b4bad7b319fdd4ebc54cb680c2b9e8d6a65
Bassman2 <ralf.beckers@hotmail.com>
2023-03-18 16:30:08 +0100

    Update

--- Src/TheTVDBWebApiShare/SeasonBaseRecord.cs
+++ Src/TheTVDBWebApiShare/SeasonBaseRecord.cs
@@ -11,6 +11,27 @@
         [JsonPropertyName("id")]
         public int Id { get; set; }
 
+        [JsonPropertyName("seriesId")]
+        public long SeriesId { get; set; }
+
+        [JsonPropertyName("type")]
+        public SeasonType Type { get; set; }
+
+        /// <summary>
+        /// Name of the season.
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("number")]
+        public long Number { get; set; }
+
+        [JsonPropertyName("nameTranslations")]
+        public List<string> NameTranslations { get; set; }
+
+        [JsonPropertyName("overviewTranslations")]
+        public List<string> OverviewTranslations { get; set; }
+
         /// <summary>
         /// Uri of a season image.
         /// </summary>
@@ -20,39 +41,19 @@
         [JsonPropertyName("imageType")]
         public int ImageType { get; set; }
 
+        [JsonPropertyName("companies")]
+        public Companies Companies { get; set; }
+
         /// <summary>
         /// Last updated date.
         /// </summary>
         [JsonPropertyName("lastUpdated")]
         public string LastUpdated { get; set; }
-
-        /// <summary>
-        /// Name of the season.
-        /// </summary>
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("nameTranslations")]
-        public List<string> NameTranslations { get; set; }
-
-        [JsonPropertyName("number")]
-        public long Number { get; set; }
-
-        [JsonPropertyName("overviewTranslations")]
-        public List<string> OverviewTranslations { get; set; }
-
-        [JsonPropertyName("companies")]
-        public Companies Companies { get; set; }
-
-        [JsonPropertyName("seriesId")]
-        public long SeriesId { get; set; }
-
-        [JsonPropertyName("type")]
-        public SeasonType Type { get; set; }
-
+        
         /// <summary>
         /// Year of the season.
         /// </summary>
+        /// <remarks>Not exists for extended Record </remarks>
         [JsonPropertyName("year")]
         public string Year { get; set; }
     }
--- Src/TheTVDBWebApiShare/SeasonExtendedRecord.cs
+++ Src/TheTVDBWebApiShare/SeasonExtendedRecord.cs
@@ -3,72 +3,25 @@
     /// <summary>
     /// Extended season record.
     /// </summary>
-    public class SeasonExtendedRecord
+    public class SeasonExtendedRecord : SeasonBaseRecord
     {
-        [JsonPropertyName("artwork")]
-        public List<ArtworkBaseRecord> Artwork { get; set; }
-
-        [JsonPropertyName("companies")]
-        public List<Companies> Companies { get; set; }
-
         [JsonPropertyName("episodes")]
         public List<EpisodeBaseRecord> Episodes { get; set; }
-
-        /// <summary>
-        /// Id of the season.
-        /// </summary>
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
-
-        /// <summary>
-        /// Uri of a season image.
-        /// </summary>
-        [JsonPropertyName("image")]
-        public string Image { get; set; }
-
-        [JsonPropertyName("imageType")]
-        public int ImageType { get; set; }
-
-        /// <summary>
-        /// Last updated date.
-        /// </summary>
-        [JsonPropertyName("lastUpdated")]
-        public string LastUpdated { get; set; }
-
-        /// <summary>
-        /// Name of the season.
-        /// </summary>
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("nameTranslations")]
-        public List<string> NameTranslations { get; set; }
-
-        [JsonPropertyName("number")]
-        public long Number { get; set; }
-
-        [JsonPropertyName("overviewTranslations")]
-        public List<string> OverviewTranslations { get; set; }
-        
-        [JsonPropertyName("seriesId")]
-        public long SeriesId { get; set; }
 
         [JsonPropertyName("trailers")]
         public List<Trailer> Trailers { get; set; }
 
-        [JsonPropertyName("type")]
-        public SeasonType Type { get; set; }
-
+        [JsonPropertyName("artwork")]
+        public List<ArtworkBaseRecord> Artwork { get; set; }
+        
+        
         [JsonPropertyName("tagOptions")]
         public List<TagOption> TagOptions { get; set; }
 
-        [JsonPropertyName("translations")]
-        public List<Translation> Translations { get; set; }
+        // not exists in json response
+        //[JsonPropertyName("translations")]
+        //public List<Translation> Translations { get; set; }
 
-        /// <summary>
-        /// Year of the season.
-        /// </summary>
-        [JsonPropertyName("year")]
-        public string Year { get; set; }
+        
     }
 }
