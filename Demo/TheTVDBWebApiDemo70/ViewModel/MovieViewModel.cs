39d9adf30b8065edef4818c56147a8421c0f88ff
Bassman2 <ralf.beckers@hotmail.com>
2023-03-18 01:09:33 +0100

    Update

--- Demo/TheTVDBWebApiDemo70/ViewModel/MovieViewModel.cs
+++ Demo/TheTVDBWebApiDemo70/ViewModel/MovieViewModel.cs
@@ -5,19 +5,23 @@
         private string apiKey => Environment.GetEnvironmentVariable("API_KEY");
         private string userKey => null; // Environment.GetEnvironmentVariable("USER_KEY");
          
-        public MovieViewModel(MovieBaseRecord movieBaseRecord)
+        public MovieViewModel(MovieBaseRecord record)
         {
-            this.MovieBaseRecord = movieBaseRecord;
+            this.MovieListRecord = record;
 
             Task.Run(async () =>
             {
                 using (var client = new TVDBWeb())
                 {
                     await client.LoginAsync(apiKey, userKey);
-                    this.MovieExtendedRecord = await client.GetMovieExtendedAsync(movieBaseRecord.Id);
+                    this.MovieBaseRecord = await client.GetMovieAsync(record.Id);
+                    this.MovieExtendedRecord = await client.GetMovieExtendedAsync(record.Id);
                 }
             });
         }
+
+        [ObservableProperty]
+        private MovieBaseRecord movieListRecord;
 
         [ObservableProperty]
         private MovieBaseRecord movieBaseRecord;
