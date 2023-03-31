3515149c0d06964af733e287ef790d1f6f9b4f9a
Bassman2 <ralf.beckers@hotmail.com>
2023-03-31 16:51:28 +0200

    Update

--- Src/TheTVDBWebApiShare/Character.cs
+++ Src/TheTVDBWebApiShare/Character.cs
@@ -6,61 +6,17 @@
     public class Character
     {
         /// <summary>
-        /// An alias model, which can be associated with a series, season, movie, person, or list.
-        /// </summary>
-        [JsonPropertyName("aliases")]
-        public List<Alias> Aliases { get; set; }
-
-        [JsonPropertyName("episode")]
-        public RecordInfo Episode { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <remarks>Can be null.</remarks>
-        [JsonPropertyName("episodeId")]
-        public int? EpisodeId { get; set; }
-
-        /// <summary>
         /// 
         /// </summary>
         /// <remarks>Can be null</remarks>
         [JsonPropertyName("id")]
         public long? Id { get; set; }
 
-        [JsonPropertyName("image")]
-        public string Image { get; set; }
-
-        [JsonPropertyName("isFeatured")]
-        public bool IsFeatured { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <remarks>Can be null.</remarks>
-        [JsonPropertyName("movieId")]
-        public int? MovieId { get; set; }
-
-        [JsonPropertyName("movie")]
-        public RecordInfo Movie { get; set; }
-
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
-        [JsonPropertyName("nameTranslations")]
-        public List<string> NameTranslations { get; set; }
-
-        [JsonPropertyName("overviewTranslations")]
-        public List<string> OverviewTranslations { get; set; }
-
         [JsonPropertyName("peopleId")]
         public int PeopleId { get; set; }
-
-        [JsonPropertyName("personImgURL")]
-        public string PersonImgURL { get; set; }
-
-        [JsonPropertyName("peopleType")]
-        public string PeopleType { get; set; }
 
         /// <summary>
         /// 
@@ -72,19 +28,63 @@
         [JsonPropertyName("series")]
         public RecordInfo Series { get; set; }
 
-        [JsonPropertyName("sort")]
-        public long Sort { get; set; }
+        [JsonPropertyName("movie")]
+        public RecordInfo Movie { get; set; }
 
-        [JsonPropertyName("tagOptions")]
-        public List<TagOption> TagOptions { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>Can be null.</remarks>
+        [JsonPropertyName("movieId")]
+        public int? MovieId { get; set; }
+
+        [JsonPropertyName("episode")]
+        public RecordInfo Episode { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>Can be null.</remarks>
+        [JsonPropertyName("episodeId")]
+        public int? EpisodeId { get; set; }
 
         [JsonPropertyName("type")]
         public long Type { get; set; }
 
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("sort")]
+        public long Sort { get; set; }
+
+        [JsonPropertyName("isFeatured")]
+        public bool IsFeatured { get; set; }
+
         [JsonPropertyName("url")]
         public string Url { get; set; }
 
+        [JsonPropertyName("nameTranslations")]
+        public List<string> NameTranslations { get; set; }
+
+        [JsonPropertyName("overviewTranslations")]
+        public List<string> OverviewTranslations { get; set; }
+
+        /// <summary>
+        /// An alias model, which can be associated with a series, season, movie, person, or list.
+        /// </summary>
+        [JsonPropertyName("aliases")]
+        public List<Alias> Aliases { get; set; }
+
+        [JsonPropertyName("peopleType")]
+        public string PeopleType { get; set; }
+
         [JsonPropertyName("personName")]
         public string PersonName { get; set; }
+
+        [JsonPropertyName("tagOptions")]
+        public List<TagOption> TagOptions { get; set; }
+
+        [JsonPropertyName("personImgURL")]
+        public string PersonImgURL { get; set; }
     }
 }
---
+++ Test/TheTVDBWebApiTestShare/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TheTVDBWebApiTestShare
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
