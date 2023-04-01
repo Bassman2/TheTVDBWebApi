c945eebe4e4fbdba95ef70ce5fdce3d4ed2f4f96
Bassman2 <ralf.beckers@hotmail.com>
2023-04-01 11:33:40 +0200

    Update

--- Test/TheTVDBWebApiTest70/TVDBWebUnitTest.Content.cs
+++ Test/TheTVDBWebApiTest70/TVDBWebUnitTest.Content.cs
@@ -21,7 +21,15 @@
             Assert.AreEqual("Suitable for all audiences", res[0].Description, "Description0");
             Assert.AreEqual("episode", res[0].ContentType, "ContentType0");
             Assert.AreEqual(0, res[0].Order, "Order0");
-            Assert.AreEqual("ATP", res[0].FullName, "FullName0");            
+            Assert.AreEqual("ATP", res[0].FullName, "FullName0");
+
+            Assert.AreEqual(2, res[1].Id, "Id1");
+            Assert.AreEqual("SAM13", res[1].Name, "Name1");
+            Assert.AreEqual("arg", res[1].Country, "Country1");
+            Assert.AreEqual("Suitable for ages 13 and up", res[1].Description, "Description1");
+            Assert.AreEqual("episode", res[1].ContentType, "ContentType1");
+            Assert.AreEqual(0, res[1].Order, "Order1");
+            Assert.AreEqual("SAM13", res[1].FullName, "FullName1");
         }
     }
 }
