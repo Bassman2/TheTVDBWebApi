bd78efd4c6de03977ea8cbd67192e9df14640d96
Bassman2 <ralf.beckers@hotmail.com>
2023-04-13 00:27:23 +0200

    Update

--- Src/TheTVDBWebApiShare/Internal/Converter/JsonEnumConverter.cs
+++ Src/TheTVDBWebApiShare/Internal/Converter/JsonEnumConverter.cs
@@ -2,62 +2,28 @@
 {
     internal class JsonEnumConverter<TEnum> : JsonConverter<TEnum> where TEnum : struct, System.Enum
     {
+        private struct Item
+        {
+            public TEnum Value;
+            public string Name;
+        }
 
-        private readonly Dictionary<TEnum, string> _enumToString = new();
-        private readonly Dictionary<string, TEnum> _stringToEnum = new();
-        private readonly Dictionary<int, TEnum> _numberToEnum = new();
+        private readonly IEnumerable<Item> items;
 
         public JsonEnumConverter()
         {
-            var type = typeof(TEnum);
-
-            foreach (var value in Enum.GetValues<TEnum>())
-            {
-                var enumMember = type.GetMember(value.ToString())[0];
-                var attr = enumMember.GetCustomAttributes(typeof(EnumMemberAttribute), false)
-                  .Cast<EnumMemberAttribute>()
-                  .FirstOrDefault();
-
-                _stringToEnum.Add(value.ToString(), value);
-                var num = Convert.ToInt32(type.GetField("value__")
-                        .GetValue(value));
-                if (attr?.Value != null)
-                {
-                    _enumToString.Add(value, attr.Value);
-                    _stringToEnum.Add(attr.Value, value);
-                    _numberToEnum.Add(num, value);
-                }
-                else
-                {
-                    _enumToString.Add(value, value.ToString());
-                }
-            }
+            this.items = Enum.GetValues<TEnum>().Select(i => new Item() { Value = i, Name = i.Value() });
         }
 
         public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var type = reader.TokenType;
-            if (type == JsonTokenType.String)
-            {
-                var stringValue = reader.GetString();
-                if (_stringToEnum.TryGetValue(stringValue, out var enumValue))
-                {
-                    return enumValue;
-                }
-            }
-            else if (type == JsonTokenType.Number)
-            {
-                var numValue = reader.GetInt32();
-                _numberToEnum.TryGetValue(numValue, out var enumValue);
-                return enumValue;
-            }
-
-            return default;
+            string name = reader.GetString();
+            return this.items.FirstOrDefault(i => i.Name == name).Value;
         }
 
         public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(_enumToString[value]);
+            writer.WriteStringValue(this.items.FirstOrDefault(i => i.Value.Equals(value)).Name);
         }
     }
 }
