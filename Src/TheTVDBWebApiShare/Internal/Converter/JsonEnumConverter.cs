ec104d0faf800c0f0b6913685022ef4d0d9fdb3b
Ralf Beckers <Ralf.Beckers@hotmail.com>
2024-12-14 13:47:50 +0100

    Update

--- Src/TheTVDBWebApiShare/Internal/Converter/JsonEnumConverter.cs
+++ Src/TheTVDBWebApiShare/Internal/Converter/JsonEnumConverter.cs
@@ -1,29 +1,29 @@
-﻿namespace TheTVDBWebApi.Internal.Converter
-{
-    internal class JsonEnumConverter<TEnum> : JsonConverter<TEnum> where TEnum : struct, System.Enum
-    {
-        private struct Item
-        {
-            public TEnum Value;
-            public string? Name;
-        }
+﻿//namespace TheTVDBWebApi.Internal.Converter
+//{
+//    internal class JsonEnumConverter<TEnum> : JsonConverter<TEnum> where TEnum : struct, System.Enum
+//    {
+//        private struct Item
+//        {
+//            public TEnum Value;
+//            public string? Name;
+//        }
 
-        private readonly IEnumerable<Item> items;
+//        private readonly IEnumerable<Item> items;
 
-        public JsonEnumConverter()
-        {
-            this.items = Enum.GetValues<TEnum>().Select(i => new Item() { Value = i, Name = i.Value() });
-        }
+//        public JsonEnumConverter()
+//        {
+//            this.items = Enum.GetValues<TEnum>().Select(i => new Item() { Value = i, Name = i.Value() });
+//        }
 
-        public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            string? name = reader.GetString();
-            return this.items.FirstOrDefault(i => i.Name == name).Value;
-        }
+//        public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+//        {
+//            string? name = reader.GetString();
+//            return this.items.FirstOrDefault(i => i.Name == name).Value;
+//        }
 
-        public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
-        {
-            writer.WriteStringValue(this.items.FirstOrDefault(i => i.Value.Equals(value)).Name);
-        }
-    }
-}
+//        public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
+//        {
+//            writer.WriteStringValue(this.items.FirstOrDefault(i => i.Value.Equals(value)).Name);
+//        }
+//    }
+//}
--- Src/TheTVDBWebApiShare/Internal/EnumMemberValue.cs
+++ Src/TheTVDBWebApiShare/Internal/EnumMemberValue.cs
@@ -2,7 +2,7 @@
 {
     internal static class EnumMemberValue
     {
-        public static string? Value<TEnum>(this TEnum enumVal)
+        public static string? Value<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]TEnum>(this TEnum enumVal) where TEnum : Enum
         {
             return typeof(TEnum).GetMember(enumVal?.ToString()!).FirstOrDefault()?.GetCustomAttributes(typeof(EnumMemberAttribute), false).Cast<EnumMemberAttribute>().FirstOrDefault()?.Value;
         }
